15c9fbd1153ca2afe508385d4571b67a316b36fe
xFreshu <lprzybyszcontact@gmail.com>
2023-03-09 14:13:20 +0100

    initial commit

--- next.config.js
+++ next.config.js
@@ -1,8 +1,14 @@
+const path = require('path')
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
     appDir: true,
   },
 }
+module.exports = {
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
+};
 
 module.exports = nextConfig
